02a02081e9a84c2713065594146ed844127b548d
David Dias <daviddias.p@gmail.com>
2015-06-29 15:19:11 -0700

    add DATA_FRAME tests

--- test/spdy/parser-test.js
+++ test/spdy/parser-test.js
@@ -23,6 +23,9 @@ describe('SPDY Parser', function() {
 
     parser.once('data', function(frame) {
       // console.log('THE FRAME \n', frame)
+      // if (frame.data) { 
+      //  console.log('hey ', frame.data.toString('hex'))
+      //}
       assert.deepEqual(frame, expected);
       assert.equal(parser.buffer.size, 0);
       done();
@@ -43,12 +46,12 @@ describe('SPDY Parser', function() {
   
   describe('SYN_STREAM', function() {
     // [x] pass with http header
-    // [ ] pass without http header
+    // [~] pass without http header (apparently it isn't supposed to - https://github.com/indutny/spdy-transport/issues/1#issuecomment-116108202
     // [ ] fail on stream ID 0
     // [ ] pass on FIN flag
     // [ ] pass on UNIDIRECIONAL flag
    
-    it('should parse SYN_STREAM frame with http header', function(done) {
+    it('should parse frame with http header', function(done) {
       var hexFrame =  '800300010000002c0000000100000000000078' +
                       'f9e3c6a7c202e50e507ab442a45a77d7105006' +
                       'b32a4804974d8cfa00000000ffff';
@@ -70,7 +73,7 @@ describe('SPDY Parser', function() {
       }, done);
     })
 
-    /* it('should parse general frame without http header', function(done) {
+    /* it('should parse frame without http header', function(done) {
       var cvt = '80030001';
       var flags = '00';
       var len = '000004';
@@ -110,30 +113,44 @@ describe('SPDY Parser', function() {
   })
 
   describe('DATA_FRAME', function() {
-    // [ ] pass with no flags
-    // [ ] pass with flag fin
-    // [ ] fail with non valid flag
+    // [x] pass with no flags
+    // [x] pass with flag fin
 
-    // DATA Stream (from client)
-    // 000000010000001157726974696e6720746f2073747265616d0000000101000000
+    it('should parse frame with no flags', function(done) {
+      var hexFrame = '000000010000001157726974696e6720746f2073747265616d'
+     
+      pass(hexFrame, {
+        "data": new Buffer('57726974696e6720746f2073747265616d', 'hex'),
+        "fin": false,
+        "id": 1,
+        "type": "DATA",
+      }, done);
+    })
 
-    // Data Stream (from server)
-    // 000000010000001157726974696e6720746f2073747265616d0000000101000000
+    it('should parse frame with FLAG_FIN', function(done) {
+      var hexFrame = '000000010100001157726974696e6720746f2073747265616d'
+     
+      pass(hexFrame, {
+        "data": new Buffer('57726974696e6720746f2073747265616d', 'hex'),
+        "fin": true,
+        "id": 1,
+        "type": "DATA",
+      }, done);
+    })
 
   })
 
   describe('RST_STREAM', function() {
     // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
-    // [ ] pass with frame without flags and status code PROTOCOL_ERROR
+    // [ ] pass with frame without flags and status code INVALID_STREAM
+    // [ ] pass with frame without flags and status code REFUSED_STREAM
+    // [ ] pass with frame without flags and status code UNSUPPORTED_VERSION
+    // [ ] pass with frame without flags and status code CANCEL
+    // [ ] pass with frame without flags and status code INTERNAL_ERROR
+    // [ ] pass with frame without flags and status code FLOW_CONTROL_ERROR
+    // [ ] pass with frame without flags and status code STREAM_IN_USE
+    // [ ] pass with frame without flags and status code STREAM_ALREADY_CLOSED
+    // [ ] pass with frame without flags and status code FRAME_TOO_LARGE
     // [ ] fail with frame without flags and status code 0 (unvalid)
     // [ ] fail with frame with flags (there should be none)
 
